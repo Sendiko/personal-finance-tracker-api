d8d7351fd0d70792c82af783d3479ad08e6a42f2
Sendiko <sarangtawon897@gmail.com>
2025-06-02 11:25:33 +0700

    feat: serve index.html

--- index.ts
+++ index.ts
@@ -2,15 +2,17 @@ import express from "express";
 import { Request, Response } from "express";
 import router from "./src/router/route";
 import { default as sync } from "./src/database/sync";
+const path = require("path");
 const app = express();
 const port = 3000;
 
 sync();
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/", (req: Request, res) => {
-  res.send("Hello World!");
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.use(router);
